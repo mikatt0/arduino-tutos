ea2a5a911ff645b6765fffcfcddc7936c80c3b3d
Mickaël LAPOUGE <mickael.lapouge@michelin.com>
2025-06-27 11:41:31 +0200

    Try to configure Github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,16 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mikatt0.github.io/arduino-tutos/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/arduino-tutos/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'mikatt0', // Usually your GitHub org/user name.
+  projectName: 'arduino-tutos', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
