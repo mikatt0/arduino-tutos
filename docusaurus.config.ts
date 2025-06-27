d037b065f73f98d79b540e9400e7729b3778d356
Mickaël LAPOUGE <mickael.lapouge@michelin.com>
2025-06-27 13:53:31 +0200

    Try to fix Github pages configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://mikatt0.github.io/arduino-tutos/',
+  url: 'https://mikatt0.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/arduino-tutos/',
+  // baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
