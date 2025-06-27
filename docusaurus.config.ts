516c67d2e8154d7b6a2cb7008776ff329ff2355e
Mickaël LAPOUGE <mickael.lapouge@michelin.com>
2025-06-27 14:11:37 +0200

    Try gh pages on main

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,8 +19,8 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/arduino-tutos/',
-  deploymentBranch : "gh-pages",
   // baseUrl: '/',
+  // deploymentBranch : "gh-pages",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
